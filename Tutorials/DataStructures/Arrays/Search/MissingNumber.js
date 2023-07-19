7ff5e63bcc20bba291f295d785d4d867a888844f
Aryan Tiwari <arutiwari500@gmail.com>
2023-07-19 17:08:04 +0530

    17 Jul

--- Tutorials/DataStructures/Arrays/Search/MissingNumber.js
+++ Tutorials/DataStructures/Arrays/Search/MissingNumber.js
@@ -5,13 +5,10 @@ function findMissing(a) {
   if (a[size - 1] != size + 1) return size + 1;
 
   var low = 0;
-  var high = a.length-1;
+  var high = a.length - 1;
   while (low <= high) {
-    console.log("Low is " + low);
-    console.log("R is " + high);
     var mid = Math.floor((low + high) / 2);
-    console.log("Mid is " + mid);
-    console.log(a[mid]);
+
     if (a[mid] - mid === 1 && a[mid + 1] - (mid + 1) === 2) return a[mid] + 1;
     if (a[mid] - mid === 1) {
       low = mid + 1;
@@ -21,5 +18,4 @@ function findMissing(a) {
   }
   return -1;
 }
-console.log("floor " + Math.floor((8 - 0) / 2));
 console.log(findMissing(a));
